dfdc17e28dfc1b9d43525b2e0392df31d4fbab02
henryzihaohuang <atlas.bb.contact@gmail.com>
2020-11-18 12:49:04 -0600

    Update slideable

--- Chess Skeleton/slideable.rb
+++ Chess Skeleton/slideable.rb
@@ -1,26 +1,78 @@
-module Slideable
-    HORIZONTAL_DIRS:[]
+# 7| -4 -2 -3 -5 -6 -3 -2 -4
+# 6| -1 -1 -1 -1 -1 -1 -1 -1
+# 5|  0  0  0  0  0  0  0  0
+# 4|  0  0  0  0  0  0  0  0
+# 3|  0  0  0  *  0  0  0  0
+# 2|  0  0  0  0  0  0  0  0  [3,3] up[3,4]   right[4,3]   left[2,3]     down[3,2]
+# 1|  1  1  1  1  1  1  1  1        up[x,y+1] right[x+1,y] left[x-1,y]   down[x,y-1]
+# 0|  4  2  3  5  6  3  2  4
+#   ------------------------- [3,3] right diag [x+1, y+1] left diag [x-1 y+1] downright [x+1, y-1] downleft [x-1 y-1]
+#     0  1  2  3  4  5  6  7
 
-    DIAGONAL_DIRS:[]
+module Slideable
+    HORIZONTAL_DIRS:[
+        [x,y+1] # up
+        [x,y-1] #down
+        [x+1,y] #right
+        [x-1,y] #left
+    ]
+
+    DIAGONAL_DIRS:[
+        [x+1, y+1] #right up
+        [x-1 y+1] #left up
+        [x+1, y-1] #right down
+        [x-1 y-1] #left down
+    ]
 
     def horizontal_dirs
-        position = Piece.position #[0,0]
-        x, y = position
-        i = 0
-        
-        while i < 8
-            new_horiz_move = board[x][y+i] unless new_horiz_move == position
-            HORIZONTAL_DIRS << new_horiz_move
-
-            new_vert_move = board[x+1][y] unless new_vert_move == position
-            HORIZONTAL_DIRS << new_vert_move
-
-            i+=1
-        end
         HORIZONTAL_DIRS
     end
 
     def diagonal_dirs
+        DIAGONAL_DIRS
+    end
+
+    def moves
+        if Piece.symbol == "♗"
+            HORIZONTAL_DIRS
+        elsif Piece.symbol == "♖"
+            DIAGONAL_DIRS
+        elsif Piece.symbol == "♕"
+            HORIZONTAL_DIRS + DIAGONAL_DIRS
+        end
+
+    end
+
+    private
+    def move_dirs
+        raise NotImplementedError # this only executes if
+    end
+
+    def grow_unblocked_moves_in_dir(dx, dy)
+        
+        
+        position = Piece.position #[0,0]
+        x, y = position
+        i = 0
+        
+        while i < 8 # within board
+            #until theres another piece 
+               # check piece color
+               #if color is the same we stop
+               
+               #if the color is not the same we consider the position too.
+
+
+
+
+            i+=1
+        end
+
+
+        HORIZONTAL_DIRS
+
+
+
         position =  Piece.position 
         i = 0
         x, y = position
@@ -34,24 +86,6 @@ module Slideable
             i+=1
         end
         DIAGONAL_DIRS
-    end
-
-    def moves
-        if Piece.symbol == "♗"
-            HORIZONTAL_DIRS
-        elsif Piece.symbol == "♖"
-            DIAGONAL_DIRS
-        elsif Piece.symbol == "♕"
-            HORIZONTAL_DIRS + DIAGONAL_DIRS
-
-
-    end
-
-    private
-    def move_dirs
-    end
-
-    def grow_unblocked_moves_in_dir(dx, dy)
     end
 
 
